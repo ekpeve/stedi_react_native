2c25d0b3f33aed68a8149702b03afe5083e46641
Edidiong Ekpe <edidiongekpeva@gmail.com>
2022-06-14 15:01:41 -0600

    Added login button

--- Login.js
+++ Login.js
@@ -1,26 +1,34 @@
 import {useState} from "react";
 import { SafeAreaView, StyleSheet, TextInput } from "react-native";
-
+import {Text, TouchableOpacity, View }from "react-native";
 const Login = () => {
   const [phoneNumber, setPhoneNumber] = useState("");
   const [oneTimePassword, setOneTimePassword] = useState(null);
-
+  const onPress = () => setCount(prevCount => prevCount + 1);
   return (
     <SafeAreaView style={styles.margin}>
       <TextInput
         style={styles.input}
         onChangeText={setPhoneNumber}
         value={phoneNumber}
-        placeHolder="801-555-1212"
+        placeholder="801-555-1212"
+        placeholderTextColor= '#4251f5'
       />
       <TextInput
         style={styles.input}
         onChangeText={setOneTimePassword}
         value={oneTimePassword}
         placeholder="1234"
+        placeholderTextColor= '#4251f5'
         keyboardType="numeric"
         secureTextEntry={true}
       />
+      <TouchableOpacity
+      style={styles.button}
+      onPress={()=>{console.log('Login button was clicked')}}
+    >
+      <Text>Login</Text>
+    </TouchableOpacity>
     </SafeAreaView>
   );
 };
@@ -34,6 +42,11 @@ const styles = StyleSheet.create({
   },
   margin:{
     marginTop:300
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10
   }
 });
 
